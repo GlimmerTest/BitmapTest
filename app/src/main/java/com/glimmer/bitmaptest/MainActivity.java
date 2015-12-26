024c5f3a44f77e3895d3cbb57484df03b62c95bd
zhushengwen <zhushengwen@iboxpay.com>
2015-12-26 11:18:20 +0800

    add show tip button event

--- app/src/main/java/com/glimmer/bitmaptest/MainActivity.java
+++ app/src/main/java/com/glimmer/bitmaptest/MainActivity.java
@@ -2,8 +2,10 @@ package com.glimmer.bitmaptest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -23,6 +25,14 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "我是丸子", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.btn_show_tip).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(new TextView(MainActivity.this), R.string.toast_tip,
+                        Snackbar.LENGTH_LONG).show();
             }
         });
 
