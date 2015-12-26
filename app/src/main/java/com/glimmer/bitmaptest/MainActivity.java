c9ed1f75bf8fba0d510cdeb17112708cf1ec82f6
yangzhiying <yangzhiying@iboxpay.com>
2015-12-26 10:58:07 +0800

    我是丸子

--- app/src/main/java/com/glimmer/bitmaptest/MainActivity.java
+++ app/src/main/java/com/glimmer/bitmaptest/MainActivity.java
@@ -2,6 +2,8 @@ package com.glimmer.bitmaptest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -14,6 +16,15 @@ public class MainActivity extends Activity {
         /**
          * 读取位图的尺寸与类型
          */
-        Toast.makeText(MainActivity.this, "我是丸子", Toast.LENGTH_SHORT).show();
+
+        Button btn_wanzi = (Button) findViewById(R.id.btn_wanzi);
+
+        btn_wanzi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "我是丸子", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
