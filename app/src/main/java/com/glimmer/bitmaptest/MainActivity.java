261174e00fdaa11ac9468040916e1076fdd7371c
zhushengwen <zhushengwen@iboxpay.com>
2015-12-26 11:33:05 +0800

    add event

--- app/src/main/java/com/glimmer/bitmaptest/MainActivity.java
+++ app/src/main/java/com/glimmer/bitmaptest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.glimmer.bitmaptest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "我是丸子", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
 
